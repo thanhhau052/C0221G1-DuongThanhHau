7ff5db6440760fa7e2636349d91aa62f5c870fa1
Your Name <you@example.com>
2021-04-13 20:13:16 +0700

    ok

---
+++ Module22/src/bai_12_java_collection_framwork/bai_tap/quan_li_san_pham/SanPham.java
@@ -0,0 +1,50 @@
+package bai_12_java_collection_framwork.bai_tap.quan_li_san_pham;
+
+public class SanPham extends QuanLiSanPham {
+    private  String name;
+    private int id;
+    private  double price;
+    SanPham(){
+        super();
+
+    }
+
+    public SanPham(String name, int id, double price) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "SanPham{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                ", price=" + price +
+                '}';
+    }
+}
