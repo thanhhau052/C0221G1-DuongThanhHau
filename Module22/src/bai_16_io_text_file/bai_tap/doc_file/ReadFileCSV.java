2f61e64c98e7d3ce1b1090eb0c2bc94b2c976252
Your Name <you@example.com>
2021-04-23 10:08:21 +0700

    ok

---
+++ Module22/src/bai_16_io_text_file/bai_tap/doc_file/ReadFileCSV.java
@@ -0,0 +1,24 @@
+package bai_16_io_text_file.bai_tap.doc_file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileCSV {
+    public static void main(String[] args) throws IOException {
+        File file = new File("src\\bai_16_io_text_file\\bai_tap\\doc_file\\bai1.csv");
+//        if (!file.exists()){
+//            file.createNewFile();
+//        }
+//        FileReader fileReader= new FileReader(file);
+        BufferedReader bufferedReader= new BufferedReader(new FileReader(file));
+        String line;
+        while ((line=bufferedReader.readLine())!=null){
+            String[] strings=line.split(",");
+
+            System.out.println(strings[strings.length-1] );
+        }
+
+    }
+}
---
+++ Module22/src/bai_16_io_text_file/test.java
@@ -0,0 +1,12 @@
+package bai_16_io_text_file;
+
+import java.io.IOException;
+
+public class test {
+    public static void main(String[] args) throws IOException {
+        int o= System.in.read();
+        System.out.println(o);
+        char i='o';
+        System.out.println((char)i);
+    }
+}
