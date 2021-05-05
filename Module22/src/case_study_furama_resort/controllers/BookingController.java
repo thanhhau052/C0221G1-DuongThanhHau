ffe858b33342de51c989b12adf95b657903eea46
Your Name <you@example.com>
2021-05-05 14:01:14 +0700

    Update

--- Module22/src/case_study_furama_resort/controllers/BookingController.java
+++ Module22/src/case_study_furama_resort/controllers/BookingController.java
@@ -27,28 +27,28 @@ public class BookingController {
         Customer booking = customers.get(numCustomer-1);
 
         System.out.println("Which service to make booking?\n"
-                +"1.\tBooking Villa\n"
-                +"2.\tBooking House\n"
-                +"3.\tBooking Room\n");
+                +   "1.\tBooking Villa\n"
+                +   "2.\tBooking House\n"
+                +   "3.\tBooking Room\n");
         switch (input.nextLine()){
             case "1":
                 ServiceController.showAllVillaServices();
                 List<Villa> villaList = FuncReadServicesFileCSV.readFileCSVVilla();
-                System.out.println(">>>>>>>>>>>>>>>> Which ID VILLA to make booking?");
+                System.out.println(" Which ID VILLA to make booking?");
                 Villa villa = villaList.get(input.nextInt() -1 );
                 booking.setService(villa);
                 break;
             case "2":
                 ServiceController.showAllHouseServices();
                 List<House> houseList = FuncReadServicesFileCSV.readFileCSVHouse();
-                System.out.println(">>>>>>>>>>>>>>>> Which ID HOUSE to make booking?");
+                System.out.println(" Which ID HOUSE to make booking?");
                 House house = houseList.get(input.nextInt() -1 );
                 booking.setService(house);
                 break;
             case "3":
                 ServiceController.showAllRoomServices();
                 List<case_study_furama_resort.modol.Room> roomList = FuncReadServicesFileCSV.readFileCSVRoom();
-                System.out.println(">>>>>>>>>>>>>>>> Which ID ROOM to make booking?");
+                System.out.println(" Which ID ROOM to make booking?");
                 Room room = roomList.get(input.nextInt()-1);
                 booking.setService(room);
                 break;
