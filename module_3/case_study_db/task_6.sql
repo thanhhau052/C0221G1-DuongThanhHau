dcd6b3dc22d4c783e9efe304187ac82805722449
Your Name <you@example.com>
2021-05-14 21:10:07 +0700

    update

--- module_3/case_study_db/task_6.sql
+++ module_3/case_study_db/task_6.sql
@@ -2,9 +2,13 @@
 -- TenLoaiDichVu của tất cả các loại Dịch vụ chưa từng được Khách hàng 
 -- thực hiện đặt từ quý 1 của năm 2019 (Quý 1 là tháng 1, 2, 3).
 use case_study_db;
-select  dv.id_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu
-from dich_vu dv
-inner join loai_dich_vu ldv on dv.id_loai_dich_vu = ldv.id_loai_dich_vu
-inner join hop_dong hd on dv.id_dich_vu = hd.id_dich_vu
+select  dich_vu.id_dich_vu,
+ dich_vu.ten_dich_vu,
+ dich_vu.dien_tich,
+ dich_vu.chi_phi_thue,
+ loai_dich_vu.ten_loai_dich_vu
+from dich_vu
+inner join loai_dich_vu  on dich_vu.id_loai_dich_vu = loai_dich_vu.id_loai_dich_vu	
+inner join hop_dong  on dich_vu.id_dich_vu = hop_dong.id_dich_vu
 group by id_dich_vu, ngay_lam_hop_dong
-having month(hd.ngay_lam_hop_dong) not in (1,2,3) and year(hd.ngay_lam_hop_dong) =2019;
+having month(hop_dong.ngay_lam_hop_dong) not in (1,2,3) and year(hop_dong.ngay_lam_hop_dong) =2019;
