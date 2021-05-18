47e3e5ca64941b2cd7bc870771f2d099f761ea63
Your Name <you@example.com>
2021-05-18 22:10:47 +0700

    fix task 15

--- module_3/case_study_db/task15.sql
+++ module_3/case_study_db/task15.sql
@@ -9,10 +9,13 @@ nv.ho_ten,
  td.trinh_do, 
  bp.ten_bo_phan,
  nv.sdt,
- nv.dia_chi
+ nv.dia_chi,
+ count(hd.id_nhan_vien)  as dem_hd
 from nhan_vien nv
 join bo_phan bp  on nv.id_bo_phan = bp.id_bo_phan
 join trinh_do td on nv.id_trinh_do = td.id_trinh_do
 join hop_dong  hd on nv.id_nhan_vien = hd.id_nhan_vien
+where (year(hd.ngay_lam_hop_dong)in (2018,2019))
 group by hd.id_nhan_vien
-having count(hd.id_nhan_vien) <= 3;
+having dem_hd   <= 3 
+order by dem_hd desc;
