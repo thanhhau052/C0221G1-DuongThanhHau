87c3966727d48855c2cf6f6a21338f9f97af443a
Your Name <you@example.com>
2021-05-18 15:28:06 +0700

    Update

--- module_3/case_study_db/task20.sql
+++ module_3/case_study_db/task20.sql
@@ -7,8 +7,14 @@
   
 use case_study_db;
 
-select  id_nhan_vien, ho_ten, email, sdt, ngay_sinh, dia_chi
+select  id_nhan_vien, 
+ho_ten,
+ email, 
+ sdt, 
+ ngay_sinh, 
+ dia_chi
 from nhan_vien 
 union all
-select id_khach_hang, ho_ten, email, sdt, ngay_sinh, dia_chi
+select id_khach_hang,
+ ho_ten, email, sdt, ngay_sinh, dia_chi
 from khach_hang
