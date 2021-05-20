ab2c4f6fedaf79ed518c764e8927f46ec2ba4a42
Your Name <you@example.com>
2021-05-20 08:15:04 +0700

    update

--- module_3/case_study_db/task17.sql
+++ module_3/case_study_db/task17.sql
@@ -14,6 +14,7 @@ from hop_dong hd
 join dich_vu dv on hd.id_dich_vu = dv.id_dich_vu
 join hop_dong_chi_tiet hdct on hd.id_hop_dong = hdct.id_hop_dong
 join dich_vu_di_kem dvdk on hdct.id_dich_vu_di_kem = dvdk.id_dich_vu_di_kem
-where hd.id_khach_hang = kh.id_khach_hang
+where year(hd.ngay_lam_hop_dong)=2019
 group by hd.id_khach_hang
-having sum(dv.chi_phi_thue+ hdct.so_luong* dvdk.gia) >= 1000);
+having sum(dv.chi_phi_thue+ hdct.so_luong* dvdk.gia) > 1000);
+select * from khach_hang
