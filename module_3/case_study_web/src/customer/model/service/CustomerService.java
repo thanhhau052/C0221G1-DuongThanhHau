93dd36057c30ad6c3fd59516f983339552891a5a
Your Name <you@example.com>
2021-05-30 02:42:03 +0700

    Update

---
+++ module_3/case_study_web/src/customer/model/service/CustomerService.java
@@ -0,0 +1,19 @@
+package customer.model.service;
+
+
+
+import customer.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
