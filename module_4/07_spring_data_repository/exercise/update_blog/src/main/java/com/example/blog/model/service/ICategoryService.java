c32b24a62f4e88373a34f98f9eb428a2a26d4d15
Your Name <you@example.com>
2021-06-23 17:37:47 +0700

    Update

--- module_4/07_spring_data_repository/exercise/update_blog/src/main/java/com/example/blog/model/service/ICategoryService.java
+++ module_4/07_spring_data_repository/exercise/update_blog/src/main/java/com/example/blog/model/service/ICategoryService.java
@@ -2,7 +2,6 @@ package com.example.blog.model.service;
 
 import com.example.blog.model.entity.Category;
 
-import java.util.Optional;
 
 public interface ICategoryService extends IGeneralService<Category> {
 }
