ac4e23ef0c7bb7155906825ff3efd64ae8bdfe5d
Your Name <you@example.com>
2021-06-29 08:25:15 +0700

    Update

---
+++ module_4/10_session_and_coockie/practice/th2_luu_ten_sd_cookie/src/main/java/com/example/model/entity/User.java
@@ -0,0 +1,43 @@
+package com.example.model.entity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+//@Entity
+public class User {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
