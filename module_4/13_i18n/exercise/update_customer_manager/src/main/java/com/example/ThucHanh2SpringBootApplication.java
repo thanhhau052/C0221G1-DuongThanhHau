46edfc881c29c5c3dfd38d06a5b31a04229b9e0c
Your Name <you@example.com>
2021-07-02 11:49:28 +0700

    Update

---
+++ module_4/13_i18n/exercise/update_customer_manager/src/main/java/com/example/ThucHanh2SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThucHanh2SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThucHanh2SpringBootApplication.class, args);
+    }
+
+}
