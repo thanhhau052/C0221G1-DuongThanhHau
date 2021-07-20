d631219d44940f622c1038d8c7d5b5be939405a1
Your Name <you@example.com>
2021-07-20 22:46:45 +0700

    Update

--- module_5/03_angular_overview/exercise/bai-tap1-angular-calculator-app/src/app/calculator/calculator.component.ts
+++ module_5/03_angular_overview/exercise/bai-tap1-angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
+  public title = 'Calculator';
+  public result: any;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
+
+
+  public calculate(first: number, second: number, operator: string): number{
+    // tslint:disable-next-line:radix
+    const firstNumber: number = parseInt(first.toLocaleString());
+    // tslint:disable-next-line:radix
+    const secondNumber: number = parseInt(second.toLocaleString()) ;
+    switch (operator) {
+      case '+':
+        return this.result = firstNumber + secondNumber;
+      case '-':
+        return this.result = firstNumber - secondNumber;
+      case '*':
+        return this.result = firstNumber * secondNumber;
+      case '/':
+        return this.result = firstNumber / secondNumber;
+    }
+  }
+
 
 }
