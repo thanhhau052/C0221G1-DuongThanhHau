40e7ae72777af9e474694fa387071a90cb792885
Your Name <you@example.com>
2021-07-20 15:10:53 +0700

    Update

---
+++ module_5/02_typescript/exercise/fibonaci/src/main.ts
@@ -0,0 +1,42 @@
+// function checkNumberInFibonacci(number:number):boolean {
+//     let num = 0;
+//     let check = false;
+//     let a = 0;
+//     let b = 1;
+//     while (num <= number) {
+//         if (num == number) {
+//             check = true;
+//         }
+//         num = a;
+//         a += b;
+//         b = num;
+//     }
+//     return check
+// }
+// let n=10;
+// let sum=0;
+// for (let i =0; i<n;i++  ){
+//     if (checkNumberInFibonacci(i)){
+//         console.log(i+" ");
+//         sum+=i;
+//     }
+//
+// }
+// console.log(sum);
+function isFibonacci(number:number):number {
+    if (number<=1){
+        return 1;
+    }else {
+        return isFibonacci(number-1) + isFibonacci(number-2)
+    }
+
+}
+
+let  sum=0;
+let n=10;
+console.log("Day so fibonacci la : ")
+for (let i=0;i<n;i++){
+    console.log(isFibonacci(i))
+    sum+=isFibonacci(i)
+}
+console.log("Tong fibonacci la :"+sum)
