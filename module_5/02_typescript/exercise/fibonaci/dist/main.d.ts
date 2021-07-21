523e03f01656f08123c1b2af6c3ac3a129c25342
Your Name <you@example.com>
2021-07-21 23:01:24 +0700

    Update

---
+++ module_5/02_typescript/exercise/fibonaci/dist/main.d.ts
@@ -0,0 +1,3 @@
+declare function isFibonacci(number: number): number;
+declare let sum: number;
+declare let n: number;
---
+++ module_5/04/practice/angular-hackernews-app/src/app/navar/navar.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavarComponent } from './navar.component';
+
+describe('NavarComponent', () => {
+  let component: NavarComponent;
+  let fixture: ComponentFixture<NavarComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavarComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
