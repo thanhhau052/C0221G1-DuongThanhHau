b043347d93e7f70fd63ac67b2f99aaae405dec28
Your Name <you@example.com>
2021-07-22 19:45:29 +0700

    Update

--- module_5/02_typescript/exercise/fibonaci/src/main.ts
+++ module_5/02_typescript/exercise/fibonaci/src/main.ts
@@ -1,45 +1,17 @@
-// function checkNumberInFibonacci(number:number):boolean {
-//     let num = 0;
-//     let check = false;
-//     let a = 0;
-//     let b = 1;
-//     while (num <= number) {
-//         if (num == number) {
-//             check = true;
-//         }
-//         num = a;
-//         a += b;
-//         b = num;
-//     }
-//     return check
-// }
-// let n=10;
-// let sum=0;
-// for (let i =0; i<n;i++  ){
-//     if (checkNumberInFibonacci(i)){
-//         console.log(i+" ");
-//         sum+=i;
-//     }
-//
-// }
-// console.log(sum);
-
-
-
 // su dung  de quy
-function isFibonacci(number:number):number {
-    if (number<=1){
+function isFibonacci(number: number): number {
+    if (number <= 1) {
         return 1;
-    }else {
-        return isFibonacci(number-1) + isFibonacci(number-2)
+    } else {
+        return isFibonacci(number - 1) + isFibonacci(number - 2)
     }
 }
 
-let  sum=0;
-let n=15;
+let sum = 0;
+let n = 15;
 console.log("Day so fibonacci la : ")
-for (let i=0;i<n;i++){
+for (let i = 0; i < n; i++) {
     console.log(isFibonacci(i))
-    sum+=isFibonacci(i)
+    sum += isFibonacci(i)
 }
-console.log("Tong fibonacci la :"+sum)
+console.log("Tong fibonacci la :" + sum)
